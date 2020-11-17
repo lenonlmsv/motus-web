755141ceb36e3f0c6e0305689415f3b9d2cebb66
Jonatas Handam <jon.handam@gmail.com>
2020-11-17 15:13:12 -0300

    testando commit

---
+++ src/pages/login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Login() {
+	return (
+		<view>
+			<p>Login</p>
+			<input label="usuário"></input>
+			<p>Senha</p>
+			<input></input>
+		</view>
+	);
+}
+
+export default Login;
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 
 //Routes
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //Pages
-import Opportunities from './pages/opportunities';
+import Opportunities from "./pages/opportunities";
+import Login from "./pages/login";
 
 function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Opportunities}/>
-            </Switch>
-        </BrowserRouter>
-    )
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact component={Opportunities} />
+				<Route path="/Login" exact component={Login} />
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default Routes;
