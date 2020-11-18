ab99b196aec2a8eda9b619eaadb734095494079c
Jonatas Handam <jon.handam@gmail.com>
2020-11-18 10:51:54 -0300

    Correção de erro

--- src/routes.js
+++ src/routes.js
@@ -4,33 +4,24 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //Pages
-<<<<<<< HEAD
-import Opportunities from './pages/opportunities';
-import OpportunitieDetail from './pages/opportunitie-details'
-
-function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/oportunidades" exact component={Opportunities}/>
-                <Route path="/oportunidades/:id" exact component={OpportunitieDetail}/>
-            </Switch>
-        </BrowserRouter>
-    )
-=======
 import Opportunities from "./pages/opportunities";
+import OpportunitieDetail from "./pages/opportunitie-details";
 import Login from "./pages/login";
 
 function Routes() {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path="/" exact component={Opportunities} />
+				<Route path="/oportunidades" exact component={Opportunities} />
+				<Route
+					path="/oportunidades/:id"
+					exact
+					component={OpportunitieDetail}
+				/>
 				<Route path="/Login" exact component={Login} />
 			</Switch>
 		</BrowserRouter>
 	);
->>>>>>> 755141ceb36e3f0c6e0305689415f3b9d2cebb66
 }
 
 export default Routes;
