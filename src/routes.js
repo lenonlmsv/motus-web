143395d27a61fe2c93e1287245dba67820df77b7
Lenon  Manhães <lenon.manhaes@sysmanager.com.br>
2020-11-18 14:31:21 -0300

    Mudanças Lenon 18/11/2020

--- src/routes.js
+++ src/routes.js
@@ -21,27 +21,6 @@ function Routes() {
                 <Route path="/gravar-video" componente={VideoRecord}/>
             </Switch>
         </BrowserRouter>
-    )
-import Opportunities from "./pages/opportunities";
-import OpportunitieDetail from "./pages/opportunitie-details";
-import Login from "./pages/login";
-import VideoResume from "./pages/video-resume";
-import VideoRecord from "./pages/video-recorder";
-
-function Routes() {
-	return (
-		<BrowserRouter>
-			<Switch>
-				<Route path="/oportunidades" exact component={Opportunities} />
-				<Route
-					path="/oportunidades/:id"
-					exact
-					component={OpportunitieDetail}
-				/>
-				<Route path="/Login" exact component={Login}/>
-			</Switch>
-		</BrowserRouter>
-	);
+	)
 }
-
 export default Routes;
