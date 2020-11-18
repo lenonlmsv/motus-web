75bc0f40719bbfe7dd272c970350add3bd8562de
Jonatas Handam <jon.handam@gmail.com>
2020-11-18 14:45:32 -0300

    Correção de bug

--- src/routes.js
+++ src/routes.js
@@ -3,24 +3,37 @@ import React from "react";
 //Routes
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+//Context
+import { Provider as AuthProvider } from "./context/AuthContext";
+
 //Pages
-import Opportunities from './pages/opportunities';
-import OpportunitieDetail from './pages/opportunitie-details'
-import Login from './pages/login'
+import Opportunities from "./pages/opportunities";
+import OpportunitieDetail from "./pages/opportunitie-details";
+import Login from "./pages/login";
 import VideoResume from "./pages/video-resume";
 import VideoRecord from "./pages/video-recorder";
 
 function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/login" component={Login}/>
-                <Route path="/oportunidades" exact component={Opportunities}/>
-                <Route path="/oportunidades/:id" component={OpportunitieDetail}/>
-                <Route path="/video-curriculo" component={VideoResume}/>
-                <Route path="/gravar-video" componente={VideoRecord}/>
-            </Switch>
-        </BrowserRouter>
-	)
+	return (
+		<BrowserRouter>
+			<AuthProvider>
+				<Switch>
+					<Route path="/login" component={Login} />
+					<Route
+						path="/oportunidades"
+						exact
+						component={Opportunities}
+					/>
+					<Route
+						path="/oportunidades/:id"
+						component={OpportunitieDetail}
+					/>
+					<Route path="/video-curriculo" component={VideoResume} />
+					<Route path="/gravar-video" componente={VideoRecord} />
+				</Switch>
+			</AuthProvider>
+		</BrowserRouter>
+	);
 }
+
 export default Routes;
