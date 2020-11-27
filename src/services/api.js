3bbfeda693b61fa8da470b925c54b6a099058aa6
Jonatas Handam Santos <jonatas.santos@sysmi.com.br>
2020-11-27 17:11:54 -0300

    Tela de oportunidades pegando oportunidades da api e funcionando

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,23 @@
-import axios from 'axios';
-import {getToken} from './auth'
+import axios from "axios";
+import { getToken } from "./auth";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_ENDPOINT
-})
+	baseURL: "http://localhost:8000/",
+});
 
-api.defaults.headers.post['Content-Type'] = 'application/json';
+api.defaults.headers.post["Content-Type"] = "application/json";
 
-api.interceptors.request.use(async config => {
-    const token = getToken();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+api.interceptors.request.use(async (config) => {
+	const token = getToken();
+	if (token) {
+		console.log("Tem token");
+		config.headers.Authorization = `Bearer ${token}`;
+	} else {
+		console.log("Não tem token");
+	}
+	return config;
 });
 
 export default api;
