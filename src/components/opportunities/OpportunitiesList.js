73d9d999044ac9a99f66968ac2124f8f0257cb20
Jonatas Handam Santos <jonatas.santos@sysmi.com.br>
2020-12-01 10:55:52 -0300

    Paginação funcionando em tela de oportunidades, mas sem estilização ainda

--- src/components/opportunities/OpportunitiesList.js
+++ src/components/opportunities/OpportunitiesList.js
@@ -4,8 +4,8 @@ import OpportunitiesCard from "./OpportunitiesCard";
 import api from "../../services/api";
 import OpportunitiesEmpty from "./OpportunitiesEmpty";
 
-const OpportunitiesList = () => {
-	const [opportunities, setOpportunities] = useState([]);
+const OpportunitiesList = ({ opportunities, loading }) => {
+	/*const [opportunities, setOpportunities] = useState([]);
 	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
@@ -22,8 +22,13 @@ const OpportunitiesList = () => {
 
 		fetchOpportunities();
 	}, []);
+*/
 
-	return opportunities !== "undefined" && opportunities.length > 0 ? (
+	console.log("Na lista de oportunidades: " + opportunities);
+	console.log("Na lsita de opp, valor loading: " + loading);
+	return loading ? (
+		<div>Carregando</div>
+	) : opportunities !== "undefined" && opportunities.length > 0 ? (
 		opportunities.map((array) => {
 			return (
 				<div key={array.id}>
