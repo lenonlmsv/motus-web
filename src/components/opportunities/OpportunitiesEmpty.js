8194bb1597a81b545d27e8e5df47fd95121f3408
Jonatas Handam Santos <jonatas.santos@sysmi.com.br>
2020-11-30 21:26:50 -0300

    Componente OpportunitiesEmpty utlizando um css básico (necessita melhoria)

--- src/components/opportunities/OpportunitiesEmpty.js
+++ src/components/opportunities/OpportunitiesEmpty.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+//CSS
+import "./styles/opportunities-css.css";
+
 const OpportunitiesEmpty = () => {
-	return <div>Tem vaga não, parça</div>;
+	return (
+		<div>
+			<div className="div-image-opportunities"></div>
+			<div className="div-content-opportunities">Tem vaga não, parça</div>
+		</div>
+	);
 };
 
 export default OpportunitiesEmpty;
