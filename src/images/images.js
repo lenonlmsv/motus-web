7ae33cd895b047bb569abdff52a4e334786e13e4
Jonatas Handam <jon.handam@gmail.com>
2021-01-19 16:11:05 -0300

    Imagem de sem oportunidades modificada

--- src/images/images.js
+++ src/images/images.js
@@ -1,6 +1,6 @@
 import imageLoading from "./image-loading.gif";
 import imageLogo from "./image-logo.svg";
-import imageEmpty from "./image-sem-oportunidades.png";
+import imageEmpty from "./sem-oportunidades.png";
 import imageNotFound from "./image-not-found.png";
 
 export { imageLoading, imageLogo, imageEmpty, imageNotFound };
